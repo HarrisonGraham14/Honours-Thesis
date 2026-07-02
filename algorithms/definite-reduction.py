06e563e128ecc27007bc33ced1f82469e218c131
EvilDuck14 <theevilduck14@gmail.com>
2026-07-02 12:45:42 +1000

    work on composition formula

--- algorithms/definite-reduction.py
+++ algorithms/definite-reduction.py
@@ -5,17 +5,12 @@ def ReducePositiveDefinite(a, b, c):
         
         # Step 1: T reduction
         if np.abs(b) > a:
-            k = -np.floor(b / a)
+            k = np.ceil((b - a) / (2 * a))
             c = a * k * k + b * k + c
             b = 2 * a * k + b
 
         # Step 2: S reduction
-        if a > c:
-            a, b, c = c, -b, a
-    
-    # Step 3: special cases require b > 0
-    if a == abs(b) or a == c:
-        if b < 0:
+        if a > c or (a == c and b < 0):
             a, b, c = c, -b, a
 
     return a, b, c
